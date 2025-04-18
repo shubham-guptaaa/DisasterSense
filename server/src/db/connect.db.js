ba84d5c3dcc12e6e51f9251e8bd89f89afb0b430
Shubham Gupta <153124453+shubham-guptaaa@users.noreply.github.com>
2025-04-18 15:20:45 +0530

    #Update the connection string of database

--- server/src/db/connect.db.js
+++ server/src/db/connect.db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
         const connect = await mongoose.connect(process.env.MONGO_URI, {
         })
 
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${connect.connection.host}`)
         return connect
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`)
